06ebbf6f8ed71ce8d49fb1afef73d663628830f5
wangshilei <sirwsl@163.com>
2020-11-20 14:45:12 +0800

    更新枚举类

--- src/main/java/com/wsl/shoppingKill/constant/BaseEnum.java
+++ src/main/java/com/wsl/shoppingKill/constant/BaseEnum.java
@@ -5,10 +5,6 @@ package com.wsl.shoppingKill.constant;
  * @date 2020/11/10-18:33
  **/
 public class BaseEnum {
-    /**redis存储验证码**/
-    public static final String REDIS_PREFIX = "verify:user:";
-    public static final int CAPTCHA_EXPIRE_TIME = 5;
-
     /**
      * 手机号与IP
      **/
@@ -22,13 +18,34 @@ public class BaseEnum {
     public static final Integer ADMIN = 1;
     public static final Integer CUSTOMER = 0;
 
+    /**
+     * 成功失败
+     */
     public static final Integer SUCCESS = 1;
     public static final Integer MISS = 0;
 
+    /**
+     * OSS存储
+     */
     public static final String OSS_HEAD = "head/";
     public static final String OSS_ADVERTISE = "advertise/";
     public static final String OSS_SKU = "sku/";
     public static final String OSS_GOODS = "goods/";
     public static final String OSS_OTHER = "other/";
 
+    /**
+     * 3-退款退货
+     * 2-换货
+     * 1-退钱
+     */
+    public static final Integer REFUND_GOODS_MONEY = 3;
+    public static final Integer REFUND_GOODS = 2;
+    public static final Integer REFUND_MONEY = 1;
+
+    /**
+     * 0-未解决
+     * 1-已解决
+     */
+    public static final Integer UNSOLVED = 0;
+    public static final Integer SOLVED = 1;
 }
---
+++ src/main/java/com/wsl/shoppingKill/constant/RedisEnum.java
@@ -0,0 +1,12 @@
+package com.wsl.shoppingKill.constant;
+
+/**
+ * @author WangShilei
+ * @date 2020/11/20-14:44
+ **/
+public class RedisEnum {
+    /**redis存储验证码**/
+    public static final String REDIS_PREFIX = "verify:user:";
+    public static final int CAPTCHA_EXPIRE_TIME = 5;
+
+}
