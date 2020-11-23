82e8bf71938f94c59efe9e2d1d3db946eed22e5e
wangshilei <sirwsl@163.com>
2020-11-23 10:01:09 +0800

    homeservice模块暂停，service已经建立，先完成登录模块

---
+++ src/main/java/com/wsl/shoppingKill/serviceImpl/HomeServiceImpl.java
@@ -0,0 +1,11 @@
+package com.wsl.shoppingKill.serviceImpl;
+
+import com.wsl.shoppingKill.service.HomeService;
+
+/**
+ * @author WangShilei
+ * @date 2020/11/23-9:33
+ **/
+public class HomeServiceImpl implements HomeService {
+
+}
