9fbdfabebca64bd7ec1508fdfaed2b8d3ce5c02f
wangshilei <sirwsl@163.com>
2020-11-30 22:11:02 +0800

    加入跨域访问  引入jquery.form

--- src/main/java/com/wsl/shoppingKill/request/WebMvcConfig.java
+++ src/main/java/com/wsl/shoppingKill/request/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.wsl.shoppingKill.request;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,7 +16,7 @@ import javax.annotation.Resource;
 
 @Configuration
 @Slf4j
-public class WebMvcConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer  {
     /*
      * 注入 token 拦截器
      */
@@ -30,6 +31,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         // 添加自定义拦截器，并拦截对应 url
         registry.addInterceptor(interceptor).addPathPatterns("/admin/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
     }
 
 }
