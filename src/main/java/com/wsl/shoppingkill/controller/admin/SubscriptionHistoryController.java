a2154ef1ac383b06ba9407caf78896906c81cf6a
wangshilei <sirwsl@163.com>
2020-12-13 17:01:45 +0800

    订阅者历史记录对接结束

--- src/main/java/com/wsl/shoppingkill/controller/admin/SubscriptionHistoryController.java
+++ src/main/java/com/wsl/shoppingkill/controller/admin/SubscriptionHistoryController.java
@@ -3,12 +3,10 @@ package com.wsl.shoppingkill.controller.admin;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.wsl.shoppingkill.common.Result;
 import com.wsl.shoppingkill.domain.SubscriptionHistory;
+import com.wsl.shoppingkill.obj.constant.BaseEnum;
 import com.wsl.shoppingkill.obj.param.PageTimeParam;
 import com.wsl.shoppingkill.service.SubscriptionHistoryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
@@ -23,6 +21,30 @@ public class SubscriptionHistoryController {
 
     @Resource
     private SubscriptionHistoryService subscriptionHistoryService;
+
+    /**
+     * 消息推送，短信-邮件走同一个
+     * @author : WangShiLei
+     * @date : 2020/11/15 5:36 下午
+     * @param subscriptionHistory:
+     * @return com.wsl.shoppingKill.common.Result<java.lang.Boolean>
+     **/
+    @PostMapping("/newsPush")
+    public Result<Boolean> send(SubscriptionHistory subscriptionHistory){
+        if (subscriptionHistory.getType().equals(BaseEnum.PHONE)){
+            if (subscriptionHistory.getTitle().length()>12){
+                return Result.error("error","短信模板消息标题必须小于12字");
+            }
+            if (subscriptionHistory.getDetail().length()>12){
+                return Result.error("error","主体内容只需要填写时间，小于12字");
+            }
+        }
+        boolean flag = subscriptionHistoryService.sendNewsSubscription(subscriptionHistory);
+        if (flag){
+            return Result.success();
+        }
+        return Result.error();
+    }
 
 
     /**
